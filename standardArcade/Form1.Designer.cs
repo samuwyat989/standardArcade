35342d6546b0d5a523592dad01661650db2d1858
User <User@USER-PC>
2016-09-16 18:52:50 -0700

    Add project files.

---
+++ standardArcade/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace standardArcade
+{
+    partial class standardArcade
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(standardArcade));
+            this.playButton = new System.Windows.Forms.Button();
+            this.countdown = new System.Windows.Forms.Label();
+            this.go = new System.Windows.Forms.Label();
+            this.packMan = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.BackColor = System.Drawing.Color.Black;
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.ForeColor = System.Drawing.Color.White;
+            this.playButton.Location = new System.Drawing.Point(59, 26);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(153, 62);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "PLAY GAME";
+            this.playButton.UseVisualStyleBackColor = false;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            this.playButton.MouseLeave += new System.EventHandler(this.playButton_MouseLeave);
+            this.playButton.MouseHover += new System.EventHandler(this.playButton_MouseHover);
+            // 
+            // countdown
+            // 
+            this.countdown.AutoSize = true;
+            this.countdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdown.ForeColor = System.Drawing.Color.Black;
+            this.countdown.Location = new System.Drawing.Point(31, 198);
+            this.countdown.Name = "countdown";
+            this.countdown.Size = new System.Drawing.Size(133, 20);
+            this.countdown.TabIndex = 1;
+            this.countdown.Text = "Game will start in:";
+            // 
+            // go
+            // 
+            this.go.AutoSize = true;
+            this.go.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.go.ForeColor = System.Drawing.Color.Black;
+            this.go.Location = new System.Drawing.Point(115, 129);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(49, 24);
+            this.go.TabIndex = 2;
+            this.go.Text = "GO!!";
+            // 
+            // packMan
+            // 
+            this.packMan.BackColor = System.Drawing.Color.Black;
+            this.packMan.Location = new System.Drawing.Point(41, 143);
+            this.packMan.Name = "packMan";
+            this.packMan.Size = new System.Drawing.Size(75, 23);
+            this.packMan.TabIndex = 3;
+            this.packMan.Text = "START";
+            this.packMan.UseVisualStyleBackColor = false;
+            this.packMan.Click += new System.EventHandler(this.packMan_Click);
+            // 
+            // standardArcade
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.packMan);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.countdown);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "standardArcade";
+            this.Text = "standardArcade";
+            this.Load += new System.EventHandler(this.standardArcade_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label countdown;
+        private System.Windows.Forms.Label go;
+        private System.Windows.Forms.Button packMan;
+    }
+}
+
---
+++ standardArcade/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace standardArcade
+{
+    public partial class standardArcade : Form
+    {
+        public standardArcade()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            Controls.Remove(playButton);
+            packMan.Visible = false;
+            /*countdown.ForeColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            SoundPlayer player = new SoundPlayer(Properties.Resources.countdown_alert);
+            player.Play();
+            countdown.Text = "Game will start in: 3";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            player = new SoundPlayer(Properties.Resources.countdown_alert);
+            player.Play();
+            countdown.Text = "Game will start in: 2";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            player = new SoundPlayer(Properties.Resources.countdown_alert);
+            player.Play();
+            countdown.Text = "Game will start in: 1";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            player = new SoundPlayer(Properties.Resources.Air_Horn_SoundBible_com_964603082);
+            player.Play();
+            Controls.Remove(countdown);
+            BackColor = Color.Green;
+
+            go.BackColor = Color.Green;
+            go.ForeColor = Color.White;
+
+            Refresh();
+            Thread.Sleep(1000);*/
+
+            Controls.Remove(countdown);
+            Controls.Remove(go);
+
+            Graphics ramsLogo = this.CreateGraphics();
+            Pen redPen = new Pen(Color.Red, 10);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            Font ramsFont = new Font("Modern No. 20", 24, FontStyle.Bold);
+            Font cFont = new Font("Times New Roman", 120, FontStyle.Bold);
+            Font smallFont = new Font("Arial", 8);
+            //You can change the colour of a pen or brush after it has been created by using the following code: drawPen.Color = Color.Black;  drawBrush.Color = Color.Red;
+
+            //DrawEllipse(pen, x, y, width, height)
+            ramsLogo.DrawEllipse(redPen, 40, 60, 200, 150);
+            ramsLogo.FillEllipse(redBrush, 40, 60, 200, 150);
+            ramsLogo.DrawString("R A M S", ramsFont, whiteBrush, 105, 120);
+            ramsLogo.DrawString("C", cFont, whiteBrush, 30, 45);
+
+            ramsLogo.TranslateTransform(250, 190); 
+            ramsLogo.RotateTransform(270);
+            ramsLogo.DrawString("Central Rams Arcade", smallFont, whiteBrush, new Rectangle());
+            ramsLogo.ResetTransform();
+
+            Thread.Sleep(2000);
+
+            redPen.Color = Color.Blue;
+            ramsLogo.Clear(Color.Black);
+            //DrawLine (pen, x1, y1, x2, y2)
+            ramsLogo.DrawLine(redPen, 30, 40, 250, 40);
+            ramsLogo.DrawLine(redPen, 30, 100, 180, 100);
+            ramsLogo.DrawLine(redPen, 250, 35, 250, 245);
+            ramsLogo.DrawLine(redPen, 175, 95, 175, 245);
+
+            redPen.Color = Color.White;
+            ramsLogo.DrawLine(redPen, 90, 70, 100, 70);
+            ramsLogo.DrawLine(redPen, 130, 70, 140, 70);
+            ramsLogo.DrawLine(redPen, 170, 70, 180, 70);
+            ramsLogo.DrawLine(redPen, 210, 70, 220, 70);
+
+            ramsLogo.DrawLine(redPen, 215, 100, 215, 110);
+            ramsLogo.DrawLine(redPen, 215, 140, 215, 150);
+            ramsLogo.DrawLine(redPen, 215, 180, 215, 190);
+            ramsLogo.DrawLine(redPen, 215, 220, 215, 230);
+
+            redPen.Color = Color.Yellow;
+            whiteBrush.Color = Color.Yellow;
+            //FillPie(brush, x, y, width, height, startAngle, arcAngle)
+            //DrawPie (pen, x, y, width, height, startAngle, arcAngle) 
+            ramsLogo.DrawPie(redPen, 35, 55, 30, 30, 40, 260);
+            ramsLogo.FillPie(whiteBrush, 35, 55, 30, 30, 40, 260);
+
+            packMan.Visible = true;
+            packMan.ForeColor = Color.White;
+
+        }
+
+        private void playButton_MouseHover(object sender, EventArgs e)
+        {
+            playButton.BackColor = Color.Green;
+        }
+
+        private void playButton_MouseLeave(object sender, EventArgs e)
+        {
+            playButton.BackColor = Color.Black;
+        }
+
+        private void packMan_Click(object sender, EventArgs e)
+        {
+            Graphics packManGame = this.CreateGraphics();
+            Pen blackPen = new Pen(Color.Black, 10);
+            Pen yellowPen = new Pen(Color.Yellow, 10);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+            Font endFont = new Font("Modern No. 20", 16, FontStyle.Bold);
+
+            //FillPie(brush, x, y, width, height, startAngle, arcAngle)
+            //DrawPie (pen, x, y, width, height, startAngle, arcAngle)
+
+            packManGame.FillPie(blackBrush, 35, 55, 30, 30, 40, 260);
+            packManGame.DrawPie(blackPen, 35, 55, 30, 30, 40, 260);
+
+            packManGame.DrawPie(yellowPen, 70, 55, 30, 30, 40, 360);
+            packManGame.FillPie(yellowBrush, 70, 55, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 70, 55, 30, 30, 40, 360);
+            packManGame.FillPie(blackBrush, 70, 55, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            packManGame.FillPie(yellowBrush, 70, 55, 30, 30, 40, 260);
+            packManGame.DrawPie(yellowPen, 70, 55, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.FillPie(blackBrush, 70, 55, 30, 30, 40, 260);
+            packManGame.DrawPie(blackPen, 70, 55, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(yellowPen, 120, 55, 30, 30, 40, 360);
+            packManGame.FillPie(yellowBrush, 120, 55, 30, 30, 40, 360);
+           
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 120, 55, 30, 30, 40, 360);
+            packManGame.FillPie(blackBrush, 120, 55, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(yellowPen, 160, 55, 30, 30, 40, 260);
+            packManGame.FillPie(yellowBrush, 160, 55, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 160, 55, 30, 30, 40, 360);
+            packManGame.FillPie(blackBrush, 160, 55, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(yellowPen, 200, 55, 30, 30, 40, 260);
+            packManGame.FillPie(yellowBrush, 200, 55, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 200, 55, 30, 30, 40, 260);
+            packManGame.FillPie(blackBrush, 200, 55, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(yellowPen, 200, 55, 30, 30, 40, 360);
+            packManGame.FillPie(yellowBrush, 200, 55, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 200, 55, 30, 30, 40, 360);
+            packManGame.FillPie(blackBrush, 200, 55, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            //
+            //packManGame.RotateTransform(180);
+            //
+
+            packManGame.DrawPie(yellowPen, 200, 100, 30, 30, 40, 260);
+            packManGame.FillPie(yellowBrush, 200, 100, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 200, 100, 30, 30, 40, 260);
+            packManGame.FillPie(blackBrush, 200, 100, 30, 30, 40, 260);
+            
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(yellowPen, 200, 100, 30, 30, 40, 360);
+            packManGame.FillPie(yellowBrush, 200, 100, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 200, 100, 30, 30, 40, 360);
+            packManGame.FillPie(blackBrush, 200, 100, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(yellowPen, 200, 125, 30, 30, 40, 260);
+            packManGame.FillPie(yellowBrush, 200, 125, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 200, 125, 30, 30, 40, 260);
+            packManGame.FillPie(blackBrush, 200, 125, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(yellowPen, 200, 165, 30, 30, 40, 260);
+            packManGame.FillPie(yellowBrush, 200, 165, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(yellowPen, 200, 165, 30, 30, 40, 360);
+            packManGame.FillPie(yellowBrush, 200, 165, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 200, 165, 30, 30, 40, 360);
+            packManGame.FillPie(blackBrush, 200, 165, 30, 30, 40, 360);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(blackPen, 200, 180, 30, 30, 40, 260);
+            packManGame.FillPie(blackBrush, 200, 180, 30, 30, 40, 260);
+
+            Thread.Sleep(0200);
+
+            packManGame.DrawPie(yellowPen, 200, 200, 30, 30, 40, 260);
+            packManGame.FillPie(yellowBrush, 200, 200, 30, 30, 40, 260);
+
+            packManGame.DrawString("Congradulations", endFont, yellowBrush, 10, 170);
+        }
+
+
+        private void standardArcade_Load(object sender, EventArgs e)
+        {
+            packMan.Visible = false;
+        }
+    }
+}
